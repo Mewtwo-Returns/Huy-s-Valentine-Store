3d03c5f4f0e8ce2ee203f6f71420b8d29c35afd8
Amy <amyliangny@gmail.com>
2022-02-14 17:03:25 -0500

    create db schema

--- server/database.js
+++ server/database.js
@@ -1,8 +1,16 @@
 const { Pool } = require('pg');
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PG_URI = '';
+const PG_URI = process.env.DBURI;
 
 // create a new pool here using the connection string above
 const pool = new Pool({
   connectionString: PG_URI
 });
+
+module.exports = {
+  query: (text, params, callback) => {
+    return pool.query(text, params, callback);
+  },
+};
