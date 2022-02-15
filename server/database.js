3d7b0521673f289837b7eadcf73e8564abb108e1
Amy <amyliangny@gmail.com>
2022-02-14 20:37:09 -0500

    add products to db

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PG_URI = process.env.DBURI;
+const PG_URI = `${process.env.DBURI}`;
 
 // create a new pool here using the connection string above
 const pool = new Pool({
