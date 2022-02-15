5097fa0d3a6c7e35ae7e6fda4395ad0fc8b52259
Ruzeb <ruzeb1996@gmail.com>
2022-02-15 12:34:37 -0500

    finished styling of modals

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../styles/_reset.css';
+import '../styles/_base.css';
 import SignInModal from './components/Modals/SignInModal';
+import SignUpModal from './components/Modals/SignUpModal';
 
 const App = () => {
 
   return (
     <>
       <div>This is Huy</div>
-      <SignInModal onCloseButtonClick={() => console.log('todo')} />
+      <SignUpModal onCloseButtonClick={() => console.log('todo')} />
     </>
   );
 };
--- client/slices/userSlice.js
+++ client/slices/userSlice.js
@@ -15,10 +15,17 @@ export const userSlice = createSlice({
       // immutable state based off those changes
       state.name = action.payload;
     },
+    signup: (state, action) => {
+      // Redux Toolkit allows us to write "mutating" logic in reducers. It
+      // doesn't actually mutate the state because it uses the Immer library,
+      // which detects changes to a "draft state" and produces a brand new
+      // immutable state based off those changes
+      state.name = action.payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { getUsername } = userSlice.actions;
+export const { getUsername, signup } = userSlice.actions;
 
 export default userSlice.reducer;
